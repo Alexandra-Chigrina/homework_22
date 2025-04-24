bececc3e0cf7e0d376552ca73b3cdf19f443c6d7
Alexandra <a.chigrina89@gmail.com>
2025-04-24 22:31:09 +0300

    add controllers, edit html-files

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'catalog/home.html')
 
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        print("Имя:", name)
+        print("Телефон:", phone)
+        print("Сообщение:", message)
+        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
+
+    return render(request, 'catalog/contacts.html')
