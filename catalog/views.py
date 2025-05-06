13580b67e0b930c20db82d9547cb6a74935336de
Alexandra <a.chigrina89@gmail.com>
2025-05-06 17:04:29 +0300

    add product_detail fnc plus base.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -22,3 +22,9 @@ def contacts(request):
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
 
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
