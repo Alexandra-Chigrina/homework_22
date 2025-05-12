d8dc38d1b0490bb80a08fa4e626e30f590566233
Alexandra <a.chigrina89@gmail.com>
2025-05-12 15:04:40 +0300

    convert from FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-
+from catalog.views import ProductListView, ContactView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 from catalog.apps import CatalogConfig
-from catalog.views import add_product, contacts, home, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/<int:pk>/", product_detail, name="product_detail"),
-    path("product/add_product/", add_product, name="add_product"),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("contacts/", ContactView.as_view(), name="contacts"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,69 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from catalog.models import Category, Product
 
 
-def home(request):
-    print(Product.objects.order_by("-created_at")[:5])
-    product_list = Product.objects.order_by("-created_at")
-    paginator = Paginator(product_list, 6)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 6
 
-    context = {"page_obj": page_obj}
-    return render(request, "catalog/home.html", context)
+    def get_queryset(self):
+        return Product.objects.order_by("-created_at")
 
 
-def contacts(request):
-    if request.method == "POST":
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ["name", "description", "image", "category", "price"]
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ["name", "description", "image", "category", "price"]
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        if self.request.POST.get("delete_image") and self.object.image:
+            self.object.image.delete(save=True)
+
+        return response
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ContactView(View):
+    def get(self, request):
+        return render(request, "catalog/contacts.html")
+
+    def post(self, request):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
@@ -25,27 +71,5 @@ def contacts(request):
         print("Имя:", name)
         print("Телефон:", phone)
         print("Сообщение:", message)
+
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
-
-    return render(request, "catalog/contacts.html")
-
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "catalog/product_detail.html", context)
-
-
-def add_product(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        description = request.POST.get("description")
-        image = request.FILES.get("image")
-        category_id = request.POST.get("category")
-        price = request.POST.get("price")
-
-        Product.objects.create(name=name, description=description, image=image, category_id=category_id, price=price)
-        return HttpResponse("Товар успешно добавлен!")
-
-    categories = Category.objects.all()
-    return render(request, "catalog/add_product.html", {"categories": categories})
