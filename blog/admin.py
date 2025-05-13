393fc32725401ddb2e18cda953e44917998113e4
Alexandra <a.chigrina89@gmail.com>
2025-05-13 16:00:41 +0300

    add views_counter, blog_filter

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ from blog.models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "is_published", "created_at")
+    list_display = ("id", "title", "is_published", "created_at", "views_counter")
     list_filter = ("is_published", "created_at")
     search_fields = ("title", "content")
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from blog.models import Blog
 
 
@@ -17,6 +17,11 @@ class BlogDetailView(DetailView):
     template_name = 'blog/blog_detail.html'
     context_object_name = 'post'
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
 
 class BlogListView(ListView):
     model = Blog
@@ -24,15 +29,17 @@ class BlogListView(ListView):
     context_object_name = 'posts'
     paginate_by = 1
 
-    # def get_queryset(self):
-    #     return Blog.objects.filter(is_published=True).order_by("-created_at")
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True).order_by("-created_at")
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ["title", "content", "preview", "is_published"]
     template_name = "blog/blog_form.html"
-    success_url = reverse_lazy("blog:blog_list")
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", kwargs={"pk": self.object.pk})
 
 
 class BlogDeleteView(DeleteView):
