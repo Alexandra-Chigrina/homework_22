1760275c16b55cbf0bc460afcb93fe83fdd1b1ec
Alexandra <a.chigrina89@gmail.com>
2025-06-02 14:14:58 +0300

    add ProductForm

---
+++ catalog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from catalog.models import Product
+from django.core.exceptions import ValidationError
+
+forbidden_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар"]
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "description", "image", "category", "price"]
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if any(word in name.lower() for word in forbidden_words):
+            raise ValidationError("Название не должно содержать запрещенные слова.")
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if any(word in description.lower() for word in forbidden_words):
+            raise ValidationError("Описание не должно содержать запрещенные слова.")
+        return description
